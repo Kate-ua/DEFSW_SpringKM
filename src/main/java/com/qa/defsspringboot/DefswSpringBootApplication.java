ca5aa1ba3a0b7dbfe44aa7639c51ab8e16e78c73
Kate-ua <lesnaya_k@hotmail.com>
2022-07-05 09:22:28 +0100

    first commit

---
+++ src/main/java/com/qa/defsspringboot/DefswSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.qa.defsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefswSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefswSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/defsspringboot/DefswSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.defsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefswSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
